fd17dadec97ce0e4038afb15c1c7e3b8540ed1f1
Isabella Torres <isabellahtorress@gmail.com>
2024-08-02 12:05:36 -0300

    criando um menu

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,26 @@
 #include "../inc/manipulacao.h"
 
 int main(){
+int whilebreak = 0;
+    char resp[1];
 
-    realizarConsultaData();
+    while (whilebreak == 0)
+    {
+        cout << "------------------------------------------------" << endl;
+        cout << "Deseja realizar uma busca? (s/n)" << endl;
+        cin >> resp[1];
+        if (resp[1] == 's' || resp[1] == 'S')
+        {
+            realizarConsultaData();
+        }
+        else if (resp[1] == 'n' || resp[1] == 'N')
+        {
+            whilebreak = 1;
+            cout << "Busca finalizada" << endl;
+        }
+        else
+            cout << "Resposta incorreta" << endl;
+    }
 
     return 0;
 }
